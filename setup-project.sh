cce9790aca701385fd7917ac6403816074a7e2b2
Jedai Saboteur <paradox.inversion@gmail.com>
2022-06-30 12:19:40 -0700

    Updated paths in setup-project

--- setup-project.sh
+++ setup-project.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Setup Project
-git clone https://github.com/paradoxinversion/containerized-node-app-helm-chart.git
-helm package containerized-node-app-helm-chart
+helm package ./charts/demo-svc
 sudo echo PROJECTNAME=demo >> $BASHRC
 sudo echo SERVICENAME=demo-svc >> $BASHRC
-sudo echo HELM_CHART=./demo-0.1.0 >> $BASHRC
+sudo echo HELM_CHART=./charts/demo-svc-0.1.0 >> $BASHRC
 source $BASHRC
 
 keptn create project $PROJECTNAME --shipyard="./configs/shipyard.yaml"

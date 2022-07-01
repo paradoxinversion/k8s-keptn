ac5e081f7b4a59139853778cc8acb3f24422e3be
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-01 11:36:05 -0700

    Updated setup for jmeter

--- setup-jmeter.sh
+++ setup-jmeter.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-if [[ -z "$PROJECTNAME" ]]; then
-  PROJECTNAME=$1
-fi
-if [[ -z "$SERVICENAME" ]]; then
-  SERVICENAME=$2
-fi
-if [[ -z "$KEPTN_STAGE" ]]; then
-  KEPTN_STAGE=$3
-fi
-
-if [[ -z "$PROJECTNAME" || -z "$SERVICENAME" || -z "$KEPTN_STAGE" ]]; then
-  echo "You have to either set KEPTN_PROJECT, KEPTN_STAGE & KEPTN_SERVICE or pass them as arguments"
-  echo "usage: ./add_resources.sh simplenodeproject staging simplenode [basic|perftest]"
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+  echo "You have to either set KEPTN_PROJECT, KEPTN_SERVICE & KEPTN_STAGE or pass them as arguments"
+  echo "usage: ./setup-jmeter.sh demo demo-svc staging"
   exit 1
 fi
+
+PROJECTNAME=$1
+SERVICENAME=$2
+KEPTN_STAGE=$3
 
 keptn add-resource --project=$PROJECTNAME --service=$SERVICENAME --stage=$KEPTN_STAGE --resource=jmeter/jmeter.conf.yaml --resourceUri=jmeter/jmeter.conf.yaml
 keptn add-resource --project=$PROJECTNAME --service=$SERVICENAME --stage=$KEPTN_STAGE --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx

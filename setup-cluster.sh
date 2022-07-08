f32060c1f767a0caf064f49f065df55f720280a7
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-08 11:30:29 -0700

    Fixed kubeconfig not exporting to bashrc

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -4,19 +4,20 @@
 export BASHRC=~/.bashrc
 export KUBECTL_VERSION=1.22.6
 export K8S_VERSION=1.22.6
-source $BASHRC
+
 # Install kubectl
-echo "Downloading kubectl version ${KUBECTL_VERSION}"
+echo "Downloading kubectl version $KUBECTL_VERSION"
 curl -LO https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/amd64/kubectl
-echo "Installing kubectl version ${KUBECTL_VERSION}"
+echo "Installing kubectl version $KUBECTL_VERSION"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install k3s cluster
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v$K8S_VERSION+k3s1 K3S_KUBECONFIG_MODE="644" sh -s - --no-deploy=traefik
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-echo "export KUBECTL_VERSION=${KUBECTL_VERSION}" >> $BASHRC
-echo "export K8S_VERSION=${K8S_VERSION}" >> $BASHRC
+echo "export KUBECTL_VERSION=$KUBECTL_VERSION" >> $BASHRC
+echo "export K8S_VERSION=$K8S_VERSION" >> $BASHRC
+echo "export KUBECONFIG=$KUBECONFIG" >> $BASHRC
 source $BASHRC
 echo "The next script to run is setup-helm.sh"
 

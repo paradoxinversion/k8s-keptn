89f05cf7da712325a1fea579c5c146fc3eba88ea
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-08 11:05:09 -0700

    Updated setup-cluster

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Set initial setup variables. We copy them here to .bashrc for easier reuse. Further env vars will be gathered and added to bashrc throughout the run of the script as necessary.
-BASHRC=~/.bashrc
-sudo echo export KUBECTL_VERSION=1.22.6 >> $BASHRC
-sudo echo export K8S_VERSION=1.22.6 >> $BASHRC
-source $BASHRC
+export BASHRC=~/.bashrc
+export KUBECTL_VERSION=1.22.6
+export K8S_VERSION=1.22.6
 
 # Install kubectl
 echo "Downloading kubectl version ${KUBECTL_VERSION}"
@@ -14,8 +13,10 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install k3s cluster
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v$K8S_VERSION+k3s1 K3S_KUBECONFIG_MODE="644" sh -s - --no-deploy=traefik
-sudo echo export KUBECONFIG=/etc/rancher/k3s/k3s.yaml >> $BASHRC
-source $BASHRC
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+echo "export KUBECTL_VERSION=${KUBECTL_VERSION}" >> $BASHRC
+echo "export K8S_VERSION=${K8S_VERSION}" >> $BASHRC
+source $BASHRC
 echo "The next script to run is setup-helm.sh"
 

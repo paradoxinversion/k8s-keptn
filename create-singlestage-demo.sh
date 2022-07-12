aded4d91ce2e20f5da02aab690e9cb1c061ccffc
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-12 14:05:43 -0700

    added missing filetype to resourceUri in singlestage demo

--- create-singlestage-demo.sh
+++ create-singlestage-demo.sh
@@ -4,5 +4,5 @@
 helm package ./charts/demo-svc
 keptn create project demo --shipyard ./configs/shipyard-singlestage.yaml
 keptn create service demo-svc --project demo
-keptn add-resource --project demo --service demo-svc --all-stages --resource ./demo-svc-0.1.0.tgz --resourceUri helm/demo-svc
+keptn add-resource --project demo --service demo-svc --all-stages --resource ./demo-svc-0.1.0.tgz --resourceUri helm/demo-svc.tgz
 keptn trigger delivery --project demo --service demo-svc --image paradoxinversion/containerized-node-app:latest

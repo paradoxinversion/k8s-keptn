2f8c0a1cb0279a3abced894da58d44545f79f343
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-12 13:07:41 -0700

    added prescribed demo project creation and updated demo-svc chart

---
+++ create-singlestage-demo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# NOTE: Because of git upstream requirements, this script will not work with Keptn > 0.15.x
+# This can be remedied by adding --git-user, --git-token, and --git-remote-url to keptn create project command
+helm package ./charts/demo-svc
+keptn create project demo --shipyard ./configs/shipyard-singlestage.yaml
+keptn create service demo-svc --project demo
+keptn add-resource --project demo --service demo-svc --all-stages --resource ./demo-svc-0.1.0.tgz --resourceUri helm/demo-svc
+keptn trigger delivery --project demo --service demo-svc --image paradoxinversion/containerized-node-app:latest

c698154bc85fe236d330b801bd92d76795b2e886
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-12 15:03:43 -0700

    Updated configs

--- create-singlestage-demo.sh
+++ create-singlestage-demo.sh
@@ -3,6 +3,16 @@
 # This can be remedied by adding --git-user, --git-token, and --git-remote-url to keptn create project command
 helm package ./charts/demo-svc
 keptn create project demo --shipyard ./configs/shipyard-singlestage.yaml
+cat <<EOF | kubectl apply -f -
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: lighthouse-config-demo
+  namespace: keptn
+data:
+  sli-provider: "prometheus"
+EOF
 keptn create service demo-svc --project demo
 keptn add-resource --project demo --service demo-svc --all-stages --resource ./demo-svc-0.1.0.tgz --resourceUri helm/demo-svc.tgz
 keptn trigger delivery --project demo --service demo-svc --image paradoxinversion/containerized-node-app:latest
+

a28c80fa131d16f3c3e8204f45f1f6a208594cf8
Jedai Saboteur <paradox.inversion@gmail.com>
2022-07-12 15:08:33 -0700

    updated singlestage demo script

--- create-singlestage-demo.sh
+++ create-singlestage-demo.sh
@@ -3,6 +3,8 @@
 # This can be remedied by adding --git-user, --git-token, and --git-remote-url to keptn create project command
 helm package ./charts/demo-svc
 keptn create project demo --shipyard ./configs/shipyard-singlestage.yaml
+keptn create service demo-svc --project demo
+keptn configure monitoring prometheus --project demo --service demo-svc
 cat <<EOF | kubectl apply -f -
 kind: ConfigMap
 apiVersion: v1
@@ -12,7 +14,9 @@ metadata:
 data:
   sli-provider: "prometheus"
 EOF
-keptn create service demo-svc --project demo
 keptn add-resource --project demo --service demo-svc --all-stages --resource ./demo-svc-0.1.0.tgz --resourceUri helm/demo-svc.tgz
-keptn trigger delivery --project demo --service demo-svc --image paradoxinversion/containerized-node-app:latest
 
+keptn add-resource --project demo --stage dev --service demo-svc --resource ./configs/sli-config-prometheus.yaml --resourceUri prometheus/sli.yaml
+keptn add-resource --project demo --service demo-svc --stage dev --resource ./configs/slo-quality-gates.yaml --resourceUri slo.yaml
+
+keptn trigger delivery --project demo --service demo-svc --image paradoxinversion/containerized-node-app:latest

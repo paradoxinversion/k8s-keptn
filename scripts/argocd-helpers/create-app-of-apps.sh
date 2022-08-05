2ca6a2c4117f7dcdbb484a3cdf5fd610b77f886b
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-05 12:01:03 -0700

    Updated create-app-of-apps
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- scripts/argocd-helpers/create-app-of-apps.sh
+++ scripts/argocd-helpers/create-app-of-apps.sh
@@ -1,6 +1,12 @@
+# Setup app of apps and sync
 argocd app create applications \
     --dest-namespace argocd \
     --dest-server https://kubernetes.default.svc \
     --repo https://github.com/paradoxinversion/cna-helm-chart-repo.git \
     --path app-of-apps  
-argocd app sync applications  
+argocd app sync applications
+
+# Setup apps within app of apps
+# Make sure to create namespaces for each before syncing
+kubectl create namespace containerized-node-app
+argocd app sync containerized-node-app  

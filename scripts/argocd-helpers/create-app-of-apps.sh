d12b46a534d49018026a908654102541d8b004fa
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-11 10:26:46 -0700

    Removed demo-argo, updates app-of-apps script

--- scripts/argocd-helpers/create-app-of-apps.sh
+++ scripts/argocd-helpers/create-app-of-apps.sh
@@ -9,4 +9,23 @@ argocd app sync applications
 # Setup apps within app of apps
 # Make sure to create namespaces for each before syncing
 kubectl create namespace containerized-node-app
-argocd app sync containerized-node-app  
+kubectl label namespace containerized-node-app istio-injection=enabled --overwrite
+argocd app sync containerized-node-app
+
+kubectl apply -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: argocd-app-gateway
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: HTTP
+    hosts:
+    - "containerized-node-app.$INGRESS_HOST.nip.io"
+EOF

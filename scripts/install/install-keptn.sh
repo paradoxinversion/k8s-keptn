a754ee0a97755185c98fa174ec22e631475068a2
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-11 20:57:28 -0700

    Update install-keptn

--- scripts/install/install-keptn.sh
+++ scripts/install/install-keptn.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 export KEPTN_VERSION=0.15.1
-
+KEPTN_DOWNLOAD_FILENAME=keptn-$KEPTN_VERSION-$INSTALL_OS-$INSTALL_ARCH
 # Install Keptn CLI
-echo "Downloading, running Keptn installer version ${KEPTN_VERSION}"
-curl -sL https://get.keptn.sh | KEPTN_VERSION=$KEPTN_VERSION bash
+# Download the versioned release from github
+# keptn-[version]-[OS]-[arch].tar.gz
+curl -L https://github.com/keptn/keptn/releases/download/$KEPTN_VERSION/$KEPTN_DOWNLOAD_FILENAME.tar.gz
 
+# It needs to be unpacked
+tar -x $KEPTN_DOWNLOAD_FILENAME
+
+# Moved it into our install location
+mv $KEPTN_DOWNLOAD_FILENAME /usr/local/bin/keptn
+
+# Use Helm to Install Keptn 
 echo "Installing Keptn into the cluster"
 helm install keptn https://github.com/keptn/keptn/releases/download/$KEPTN_VERSION/keptn-$KEPTN_VERSION.tgz -n keptn --create-namespace --wait --set=continuous-delivery.enabled=true
 helm install helm-service https://github.com/keptn/keptn/releases/download/$KEPTN_VERSION/helm-service-$KEPTN_VERSION.tgz -n keptn --create-namespace --wait
 
-echo "export KEPTN_VERSION=${KEPTN_VERSION}" >> $BASHRC
+echo "export KEPTN_VERSION=${KEPTN_VERSION}" >> $RC_FILE
 
 echo "The next script to run is ./configure-istio.sh"
 
